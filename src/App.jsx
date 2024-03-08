a95a8d7edcc1cdca5a01bf4ef9b41dc3f2bc1882
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-03-08 16:17:38 +0200

    Finished Gallery layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useTransition } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import views from './assets/views.png';
-import downloads from './assets/downloads.png';
-import likes from './assets/likes.png';
+
+import ImageCard from './Components/ImageCard';
 
 function App() {
   const [images, setImages] = useState([])
   const [query, setQuery] = useState('')
   const apiKey = import.meta.env.VITE_PIXAPAY_API_KEY;
+  
+  const [loading, startTransition] = useTransition()
 
   // useEffect with no dependencies will run only once at 1st Render
   useEffect( () => {
-    fetch(`https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo`)
-    .then((res) => res.json())
-    .then((data) => {console.log(data.hits); setImages(data.hits)})
-    .catch((error) => console.error('Error fetching data:', error))}
+    startTransition(() => {
+      setLoading(true)
+      setTimeout(()=>{
+        fetch(`https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo`)
+        .then((res) => res.json())
+        .then((data) => setImages(data.hits))
+        .catch((error) => console.error('Error fetching data:', error))
+      },2000)
+      
+      setLoading(false)
+    })}
     ,[])
 
 
   return (
-    <>
-      
-        {images.map((image)=>(
-        <div className='max-w-xs rounded overflow-hidden shadow-lg'>
-
-          <img src={image.largeImageURL} alt='Random Image' className='w-full'/>
-          <div className="px-6 py-4">
-            <div className="font-bold text-purple-500 text-l mb-2">
-                Taken by {image.user}
-            </div>
-            <div className='flex flex-row'>
-              <div className='flex flex-row basis-1/3'>
-                <img src={views} alt='views icon' className='max-w-6 mx-3'/>
-                <span><strong>{image.views}</strong></span>
-              </div>
-              <div className='flex flex-row basis-1/3'>
-                <img src={downloads} alt='views icon' className='max-w-6 mx-3'/>
-                <span><strong>{image.downloads}</strong></span>
-              </div>
-              <div className='flex flex-row basis-1/3'>
-                <img src={likes} alt='views icon' className='max-w-6 mx-3'/>
-                <span><strong>{image.likes}</strong></span>
-              </div>
-            </div>
-            <div className="px-2 py-3">
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-1' >#Nature</span>
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-1' >#Motivation</span>
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-1' >#Speak</span>
-            </div>
-          </div>
-        </div>
+    <div className='container mx-auto'>
+      <div className='grid-cols-4 grid gap-5'>
+        { loading 
+            ? <h1>"Loading"</h1> 
+            : images.map((image)=>( 
+                <ImageCard key={image.id} image={image}/>
         ))}
+      </div>
         
-      
-    </>
+    </div>
   )
 }
 
---
+++ src/Components/ImageCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import views from '../assets/views.png';
+import downloads from '../assets/downloads.png';
+import likes from '../assets/likes.png';
+
+const ImageCard = ({image}) => {
+  const tags = image.tags.split(', ')
+  return (
+    <div className='max-w-xs rounded overflow-hidden shadow-lg'>
+
+          <img src={image.webformatURL} alt='Random Image' className='w-full max-h-96'/>
+          <div className="px-6 py-4">
+            <div className="font-bold text-purple-500 text-l mb-2">
+                Taken by {image.user}
+            </div>
+            <div className='flex flex-row'>
+              <div className='flex flex-row basis-1/3'>
+                <img src={views} alt='views icon' className='max-w-6 mx-3'/>
+                <span><strong>{image.views}</strong></span>
+              </div>
+              <div className='flex flex-row basis-1/3'>
+                <img src={downloads} alt='views icon' className='max-w-6 mx-3'/>
+                <span><strong>{image.downloads}</strong></span>
+              </div>
+              <div className='flex flex-row basis-1/3'>
+                <img src={likes} alt='views icon' className='max-w-6 mx-3'/>
+                <span><strong>{image.likes}</strong></span>
+              </div>
+            </div>
+            <div className="px-2 py-3">
+              {tags.map((tag, i)=>(
+              <span key={i} className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-1' >#{tag}</span>
+              ))}
+              
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default ImageCard
